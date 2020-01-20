9bab8c7780ca1f2e252d0c79be98839a90173fe1
SahilDhull <sahildhull25121998@gmail.com>
2020-01-20 21:39:16 +0530

    300d

--- config.py
+++
@@ -1,2 +0,0 @@
-glove_path = "./glove.6B"
-glove_file = "./glove.6B/glove.6B.100d.txt"
---
+++ model/config.py
@@ -0,0 +1,8 @@
+glove_path = "../glove.6B"
+glove_50 = "../glove.6B/glove.6B.50d.txt"
+glove_100 = "../glove.6B/glove.6B.100d.txt"
+glove_200 = "../glove.6B/glove.6B.200d.txt"
+glove_300 = "../glove.6B/glove.6B.300d.txt"
+
+train_file = "../datasets/train.txt"
+dev_file = "../datasets/dev.txt"
